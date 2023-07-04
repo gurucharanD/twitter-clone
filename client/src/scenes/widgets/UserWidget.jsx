a0817598c993bc333fb35713ad46a2293317cd75
gurucharan <gurucharandandyala@gmail.com>
2023-07-04 13:14:16 -0600

    refactor

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -8,7 +8,7 @@ import { Box, Typography, Divider, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserWidget = ({ userId }) => {
@@ -16,7 +16,7 @@ const UserWidget = ({ userId }) => {
   const user = useSelector(state => state.user);
   const { palette } = useTheme();
   const navigate = useNavigate();
-  const token = useSelector((state) => state.token);
+  // const token = useSelector((state) => state.token);
   const dark = palette.neutral.dark;
   const medium = palette.neutral.medium;
   const main = palette.neutral.main;
